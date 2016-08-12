2e1110ec90df57d437456d657cfde0058d30c2fe
Aswiniraja <aswinirajaeee@gmail.com>
2016-08-12 10:49:34 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -1,1 +1,25 @@
 import.java.util.*
+import java.util.Scanner;
+
+public class positive
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n<0)
+			System.out.println("negative");
+		else if(n>0)
+			System.out.println("positive");
+		else if(n==0)
+			System.out.println("zero");
+		else
+			System.out.println("invalid");
+		
+		sc.close();
+		
+
+	}
+
+}
